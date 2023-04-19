730219000d8e6d119099c9987c60269b0ef8dff5
pnoikid <rob.pena83@gmail.com>
2023-04-18 21:12:35 -0400

    made index.html and app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+//Is Truthy
+
+
+//if true, it will say i am a string
+//if false, it will say false
+let truthy = true;
+if (truthy = true) {
+    console.log("i am a string");
+}else(falsy = false){
+    console.log("false");
+}
+Boolean(false = null);
+if(false = null){
+    console.log("null");
+} else(falsy = 0){
+    console.log("0");
+    }else(" " = false){
+        console.log("falsy");
+    }
+
+    let sumNum = 50;
+    if (sumNum + 51 == 101){
+        console.log("101 > 100");
+    }
+    
+
+
