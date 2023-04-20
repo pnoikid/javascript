9164760f14e0562e74ba6d77a0ad892c304c8453
pnoikid <rob.pena83@gmail.com>
2023-04-20 08:33:16 -0400

    added code

--- app.js
+++ app.js
@@ -1,27 +1,116 @@
 //Is Truthy
 
-
-//if true, it will say i am a string
-//if false, it will say false
 let truthy = true;
 if (truthy = true) {
     console.log("i am a string");
-}else(falsy = false){
+} 
+if(falsy = false){
     console.log("false");
 }
 Boolean(false = null);
 if(false = null){
     console.log("null");
-} else(falsy = 0){
+} 
+let falsy = 0
+if (falsy = 0){
     console.log("0");
-    }else(" " = false){
+    }
+    if (" " = false){
         console.log("falsy");
     }
 
+
+    //Number Line
+
     let sumNum = 50;
     if (sumNum + 51 == 101){
-        console.log("101 > 100");
+        console.log("101 < 100");
     }
+
+    let num1 = 99;
+    let num2 = 2
+    if (num1 - num2){
+        console.log("97 > 0");
+    }
+    let main = 0;
+    let main2 = 101;
+    if (101 > 100){
+        console.log ("101");
+    }else {
+        console.log("100");
+    }
+if (500 - 500){
+    console.log("0");
+}
+else{
+    console.log("0 = 0");
+}
+
+if (-1000){
+console.log("-1000 is a negative number");
+}
+
+if (-5){
+    console.log("-5 is a negative number");
+}
+
+//Greater than 5
+
+if (6 > 5){
+    console.log("true");
+}
+
+if (11 > 10){
+    console.log("true");
+}
+
+if (0 = 0){
+    console.log("false");
+}
+
+if (1000 = -1000){
+    console.log("false");
+}
+
+if (6 > 4){
+    console.log("false");
+}
+
+if (5 = 5){
+    console.log("true");
+}
+
+//Pair and Compare
+
+if ("cat" === "cat" && 6 === "6"){
+    console.log("true");
+}
+
+if ("five" === 5 && "dog" === "dawg"){
+    console.log("false");
+}
+
+if (0 === false && "horse" === "horse"){
+    console.log("true");
+}
+
+if ("eight" === "eight" && "ate" === "ate"){
+    console.log("true");
+}
+
+if (11 === "eleven" && "four" === "for"){
+    console.log("false");
+}
+
+if ("cake" === "cake" && "pie" === "pie"){
+    console.log("true");  
+}
+
+
+
+
+
+
     
 
 
